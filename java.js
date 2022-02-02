29dbe1d50c5dbb70ce529d9da864cd598ff6798c
Luiz Fernando <luizfernandodp1103@gmail.com>
2022-02-02 09:39:22 -0300

    Primeiro commit

---
+++ java.js
@@ -0,0 +1,25 @@
+function gravarDados() {
+    if(document.getElementById('nome').value == '') {
+        alert("Preencha corretamente o cadastro!")
+        return false;
+    }
+}
+function gravarDados() {
+    if(document.getElementById('email').value == '') {
+        alert("Preencha corretamente o cadastro!")
+        return false;
+    }
+}
+function gravarDados() {
+    if(document.getElementById('telefone').value == '') {
+        alert("Preencha corretamente o cadastro!")
+        return false;
+    }
+}
+function gravarDados() {
+    if(document.getElementById('cpf').value == '') {
+        alert("Preencha corretamente o cadastro!")
+        return false;
+    }
+}
+
